a52edd97e30a2c1467a781e4fdb428850c349e10
Tomas Colazo <405477@tecnicatura.frc.edu.ar>
2024-09-11 19:44:03 -0300

    cargado de items del file.storage

--- src/app/file-owners/file-owners.component.ts
+++ src/app/file-owners/file-owners.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FileOwner } from '../../models/file.item.model';
 
 @Component({
   selector: 'app-file-owners',
   standalone: true,
   imports: [],
   templateUrl: './file-owners.component.html',
-  styleUrl: './file-owners.component.css'
+  styleUrl: './file-owners.component.css',
 })
 export class FileOwnersComponent {
-
+  @Input() owner!: FileOwner;
 }
--- src/app/file-row/file-row.component.ts
+++ src/app/file-row/file-row.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FileOwnersComponent } from '../file-owners/file-owners.component';
+import { FileItem } from '../../models/file.item.model';
+import { CommonModule, DatePipe } from '@angular/common';
+import { FileType } from '../../models/file.item.model';
 
 @Component({
   selector: 'app-file-row',
   standalone: true,
-  imports: [FileOwnersComponent],
+  imports: [FileOwnersComponent, DatePipe, CommonModule],
   templateUrl: './file-row.component.html',
   styleUrl: './file-row.component.css',
 })
-export class FileRowComponent {}
+export class FileRowComponent {
+  @Input() file!: FileItem;
+  FileType = FileType;
+}
